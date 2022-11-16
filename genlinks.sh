3493a65eb2d52db27407bf3deef87b72914b05ae
Zui-Zou <edanrohner@gmail.com>
2022-11-16 10:52:03 -0700

    sddm

--- genlinks.sh
+++ genlinks.sh
@@ -1,9 +1,11 @@
-#Creates links between repo and ~/.config through black magic
+#Creates links between repo and config files through black magic
 #
 #Again make sure all dotfiles are in ~/WeirdAssDotfiles/ or else this will not work
 #
 
-ln -sf ~/WeirdAssDotfiles/init.vim ~/.config/nvim/init.vim		#Vim Settings
-ln -sf ~/WeirdAssDotfiles/.zshrc ~/.zshrc						#OhMyZsh Settings
-ln -sf ~/WeirdAssDotfiles/kitty ~/.config/.						#Kitty Folder
+ln -sf ~/WeirdAssDotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/WeirdAssDotfiles/.zshrc ~/.zshrc
+ln -sf ~/WeirdAssDotfiles/kitty ~/.config/.
 ln -sf ~/WeirdAssDotfiles/hypr ~/.config/.
+ln -sf ~/WeirdAssDotfiles/sddm/stuff/* /usr/share/sddm/
+ln -sf ~/WeirdAssDotfiles/sddm/sddm.conf.d/default.conf /usr/lib/sddm/sddm.conf.d/default.conf
